98d6e06bff5ae09e453394f34905423cc2d36f22
MannuA19 <mannmeet.ahir@gmail.com>
2025-10-15 12:39:20 +0530

    Roboshop Terraform Code

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,8 +2,6 @@ variable "instance_type" {}
 variable "subnet_id" {}
 variable "name" {}
 variable "vpc_id" {}
-variable "allow_sg_cidr" {
-  type = "string"
-}
+variable "allow_sg_cidr" {}
 variable "allow_port" {}
 variable "env" {}
